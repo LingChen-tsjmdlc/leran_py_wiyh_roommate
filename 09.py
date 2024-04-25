896e7acdd59f288c1956b1fca5d632cd8d1436a1
祁虻 <z3247678423@qq.com>
2024-04-25 21:28:59 +0800

    uu

---
+++ 09.py
@@ -0,0 +1,13 @@
+x = int(input("请输入第一个数字："))
+y = int(input("请输入第二个数字："))
+operator=input("请输入运算符(+,-，*，/):")
+
+if operator == " + ":
+    print("x+y=",x+y)
+if operator == " - ":
+    print("x-y=",x-y)
+if operator == " * ":
+    print("x*y=",x*y)
+if operator == " / ":
+    print("x/y=",x/y)
+
