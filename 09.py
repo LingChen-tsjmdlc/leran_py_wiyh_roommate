ea1f1211fe6e29aa5f581563281e15b931e01ce5
旅程 踏上寂寞的 <3024705530@qq.com>
2024-04-25 21:37:02 +0800

    更改09.py的错误：多加了空格

--- 09.py
+++ 09.py
@@ -2,12 +2,12 @@ x = int(input("请输入第一个数字："))
 y = int(input("请输入第二个数字："))
 operator=input("请输入运算符(+,-，*，/):")
 
-if operator == " + ":
+if operator == "+":
     print("x+y=",x+y)
-if operator == " - ":
+if operator == "-":
     print("x-y=",x-y)
-if operator == " * ":
+if operator == "*":
     print("x*y=",x*y)
-if operator == " / ":
+if operator == "/":
     print("x/y=",x/y)
 
